bde55a152de4147ad3e27f2ad3d6409f1512cd9a
VAIBHAV SHUKLA <shuklavaibhav0306@gmail.com>
2020-03-07 23:56:32 +0530

    Updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const ejs = require("ejs");
 
 const app = express();
 app.set('view engine', 'ejs');
-app.use(express.static('public/index/'));
+app.use(express.static(__dirname+'/public'));
 
 app.get('/', (req, res) => {
     res.render('index');
