158607f204e97dab8d152cdfa0bb3ff32c861175
Necro Mancer <necromancer@users.noreply.github.com>
2020-03-09 21:47:57 +0530

    un

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ mongoose.connect("mongodb+srv://terminator:testdb@accounts-0uu7d.mongodb.net/Use
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-app.use(session({secret: "Shh, its a secret!"}));
+app.use(session({
+    secret: "Shh, its a secret!",
+    resave: true,
+    saveUninitialized: true
+}));
 app.set('view engine', 'ejs');
 app.use(express.static('public/index'));
 
@@ -21,36 +25,34 @@ app.use(bodyParser.urlencoded({
 const loginUsers = new mongoose.Schema({
     email: String,
     password: String,
-    fullName:String
+    fullName: String
 });
 const users = mongoose.model("user", loginUsers);
 
 const profileUsers = new mongoose.Schema({
     email: String,
     password: String,
-    fullName:String
+    fullName: String
 });
 const userPro = mongoose.model("userProfiles", profileUsers);
 
 
-function saveUser(data,res) {
+function saveUser(data, res) {
     users.findOne({
         email: data.email
-    },function(err,user){
-        if(!user)
-        {
+    }, function (err, user) {
+        if (!user) {
             const newUser = new users({
                 fullName: data.fullName,
                 email: data.email,
                 password: crypto.createHash('sha256').update(data.password).digest('hex').toString()
             });
             newUser.save();
-        }
-        else{
+        } else {
             return res.redirect("/sign-up");
         }
     });
-    
+
 }
 
 app.post('/sign-up', (req, res) => {
@@ -58,7 +60,7 @@ app.post('/sign-up', (req, res) => {
 });
 
 app.post('/save-user', (req, res) => {
-    saveUser(req.body,res);
+    saveUser(req.body, res);
     res.redirect("/loginP");
 });
 
@@ -66,6 +68,8 @@ app.post('/save-user', (req, res) => {
 
 
 app.post("/loginP", (req, res) => {
+    console.log("gLog reqest", req.session);
+
     res.sendFile(__dirname + "/signin.html");
 });
 
@@ -73,17 +77,17 @@ app.get("/loginP", (req, res) => {
     res.sendFile(__dirname + "/signin.html");
 });
 
-app.get('/', (req, res) => {
+app.get('/index', (req, res) => {
     /*if(req.session.uid)
     res.render('logout');
     else
     res.render('index');*/
-    res.render('profile');
+    res.render('index');
 });
 
 app.get('/log', (req, res) => {
     console.log("gLog reqest", req.session);
-    if(!req.session.uid)
+    if (!req.session.uid)
         return res.redirect("/loginP")
     res.render('logout');
 });
@@ -97,19 +101,21 @@ app.post('/login', (req, res) => {
             email: e
         }, function (err, user) {
             var k = 0,
-            j = 0;
+                j = 0;
             console.log(user);
-            if(!user){
-                res.send({"email":-1,"successfull": false});
-            }
-            else if (user.email === e && user.password === hvalue) {
+            if (!user) {
+                res.send({
+                    "email": -1,
+                    "successfull": false
+                });
+            } else if (user.email === e && user.password === hvalue) {
 
                 req.session.uid = user.id;
                 console.log("setting cookie", req.session, user);
                 res.send({
                     "successfull": true
                 });
-                
+
             } else {
                 if ((user.email !== e))
                     k = 1;
@@ -131,14 +137,18 @@ app.post("/logOut", (req, res) => {
 });
 
 /*---------------user search----------------------*/
-app.get("/:customListName",function(req,res){
-    userPro.findOne({email:req.params.customListName},function(err,results){
-        if(!err){
-            if(!results){
+app.get("/:customListName", function (req, res) {
+    userPro.findOne({
+        email: req.params.customListName
+    }, function (err, results) {
+        if (!err) {
+            if (!results) {
                 res.redirect("/");
-            }
-            else{
-                res.render("profile",{name: results.name,image:results.image});
+            } else {
+                res.render("profile", {
+                    name: results.name,
+                    image: results.image
+                });
             }
         }
     });
@@ -147,4 +157,23 @@ app.get("/:customListName",function(req,res){
 
 app.listen(3000, () => {
     console.log("Server started!");
+});
+
+app.get('/', (req, res)=>{
+    console.log(req.session.uid);
+    
+    res.render('profile', {id: req.session.uid});
+});
+
+app.post('/upload', function (req, res) {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+    let sampleFile = req.files.sampleFile;
+    sampleFile.mv(__dirname + '/public/'+ req.session.uid +'.jpg', function (err) {
+        if (err)
+            return res.status(500).send(err);
+
+        res.redirect('/profile');
+    });
 });
