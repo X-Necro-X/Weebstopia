a98e5bf2ff0776335a50abb2a4c48665731ab242
Necro Mancer <necromancer@users.noreply.github.com>
2020-03-10 13:16:24 +0530

    still working

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-/*------------------------Require modules------------------------*/
+// imports
 
 const express = require('express');
 const ejs = require("ejs");
@@ -8,221 +7,231 @@ const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 var crypto = require('crypto');
 
-/*------------------------Initialize Modules-------------------------*/
+// settings
 
 const app = express();
-mongoose.connect("mongodb+srv://terminator:testdb@accounts-0uu7d.mongodb.net/Users", {
+mongoose.connect("mongodb+srv://admin-necro:2634662@accounts-0uu7d.mongodb.net/Users", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 app.use(session({
-    secret: "Shh, its a secret!",
+    secret: "Anime is love, anime is life.",
     resave: true,
     saveUninitialized: true
 }));
 app.set('view engine', 'ejs');
 app.use(express.static('public/index'));
-app.use(express.static('public/upload'));
-
+app.use(express.static('public/search'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-/*--------------Create schema for mongodb---------------------*/
+// mongoDB settings
 
-const loginUsers = new mongoose.Schema({
+const logIn = new mongoose.Schema({
+    fullName: String,
+    userName: String,
     email: String,
-    password: String,
-    fullName:String,
-    image:String
+    password: String
 });
-const users = mongoose.model("user", loginUsers);
+const userLogin = mongoose.model("userLogin", logIn);
 
+// root route
 
-/*------------register user--------------------*/
+app.get('/', (req, res) => {
+    if (req.session.uid)
+        res.render('profile');
+    else
+        res.render('index');
+});
 
+// sign-up routes and functions
 
-function saveUser(data, res) {
-    users.findOne({
+app.get('/sign-up', (req, res) => {
+    res.render('sign-up', {
+        message: "Please Sign Up!"
+    });
+});
+
+app.post('/save-user', (req, res) => {
+    var result = checkUserName(req.body.userName);
+    if (result == true)
+        res.render('sign-up', {
+            message: "User name already exists!"
+        });
+    else {
+        result = saveUser(req.body);
+        if (result == true)
+            res.render('profile');
+        else
+            res.render('log-in', {
+                message: "You already have an account!"
+            });
+    }
+});
+
+function checkUserName(uName) {
+    userLogin.findOne({
+        userName: uName
+    }, (err, user) => {
+        if (user)
+            return true;
+        else
+            return false;
+    });
+}
+
+function saveUser(data) {
+    userLogin.findOne({
         email: data.email
-    }, function (err, user) {
+    }, (err, user) => {
         if (!user) {
-            const newUser = new users({
+            const newUser = new userLogin({
                 fullName: data.fullName,
+                userName: data.userName,
                 email: data.email,
                 password: crypto.createHash('sha256').update(data.password).digest('hex').toString()
             });
             newUser.save();
+            return true;
         } else {
-            return res.redirect("/sign-up");
+            return false;
         }
     });
-
 }
 
-/*--------------search user-------------------*/
+// log-in routes and functions
 
-
-app.post('/search', (req, res) => {
-    res.render('search');
-});
-
-app.post('/searchuser',(req,res)=>{
-    console.log(req.body.temp);
-    users.find({fullName: new RegExp(req.body.temp, "i")},function(err,user){
-            console.log(user);
-            res.send(user);
+app.get("/log-in", (req, res) => {
+    res.render("log-in", {
+        message: "Please Login to continue!"
     });
 });
 
-/*-------------------show profile-----------------------*/
-
-app.post('/showprofile',(req,res)=>{
-    console.log(req.body);
-    users.findOne({_id: req.body["hello"]},function(err,user){
-        res.redirect("/"+user.fullName)
-        console.log(user);
-});
-});
-
-app.get("/:customListName",function(req,res){
-    users.findOne({fullName:req.params.customListName},function(err,results){
-        if(!err){
-            if(!results){
-                res.redirect("/");
-            } else {
-                res.render("profile", {
-                    name: results.fullName,
-                    image: results.image
-                });
+app.post('/check-user', (req, res) => {
+    var uName = req.body.userName;
+    var password = req.body.pswrd;
+    var check = req.body.remember;
+    password = crypto.createHash('sha256').update(password).digest('hex').toString();
+    if (uName && password) {
+        userLogin.findOne({
+            userName: uName
+        }, (err, user) => {
+            if (user) {
+                if (user.password == password) {
+                    res.render('profile');
+                    if (check == "true") {
+                        req.session.cookie({
+                            maxAge: 365 * 24 * 60 * 60 * 1000
+                        });
+                    }
+                } else
+                    res.render('log-in', {
+                        message: "Incorrect Password!"
+                    });
             }
-        }
-    });
-    console.log(req.params.customListName);
-});
-
-
-/*---------------------sign up---------------------*/
-
-app.post('/sign-up', (req, res) => {
-    res.render('sign-up');
-});
-
-app.post('/save-user', (req, res) => {
-    saveUser(req.body, res);
-    res.redirect("/loginP");
-});
-
-/*------------------Login---------------------*/
-
-
-app.post("/loginP", (req, res) => {
-    console.log("gLog reqest", req.session);
-
-    res.sendFile(__dirname + "/signin.html");
-});
-
-app.get("/loginP", (req, res) => {
-    res.sendFile(__dirname + "/signin.html");
-});
-
-app.get('/', (req, res) => {
-    if(req.session.uid)
-    res.render('logout');
-    else
-    res.render('index');
-});
-
-app.get('/log', (req, res) => {
-    console.log("gLog reqest", req.session);
-    if (!req.session.uid)
-        return res.redirect("/loginP")
-    res.render('logout');
-});
-app.post('/login', (req, res) => {
-    const e = req.body.email;
-    const p = req.body.pname;
-    var hvalue = crypto.createHash('sha256').update(p).digest('hex').toString();
-    console.log(req.body, e, hvalue);
-    if (e && p) {
-        users.findOne({
-            email: e
-        }, function (err, user) {
-            var k = 0,
-                j = 0;
-            console.log(user);
-            if (!user) {
-                res.send({
-                    "email": -1,
-                    "successfull": false
-                });
-            } else if (user.email === e && user.password === hvalue) {
-
-                req.session.uid = user.id;
-                console.log("setting cookie", req.session, user);
-                res.send({
-                    "successfull": true
-                });
-
-            } else {
-                if ((user.email !== e))
-                    k = 1;
-                if ((user.password !== p))
-                    j = 1;
-                res.send({
-                    "email": k,
-                    "password": j,
-                    "successfull": false
-                });
+            else{
+                res.render('sign-up', { message: "You have to sign up first" });
             }
+        });
+    } else {
+        res.render('log-in', {
+            message: "Please fill both the fields!"
         });
     }
 });
 
-app.post("/logOut", (req, res) => {
+app.get("/log-out", (req, res) => {
     req.session.destroy();
     res.redirect("/");
 });
 
-/*---------------user search----------------------*/
-app.get("/:customListName", function (req, res) {
-    userPro.findOne({
-        email: req.params.customListName
-    }, function (err, results) {
-        if (!err) {
-            if (!results) {
-                res.redirect("/");
-            } else {
-                res.render("profile", {
-                    name: results.name,
-                    image: results.image
-                });
-            }
-        }
-    });
-    console.log(req.params.customListName);
-});
-/*----------starting server-----------------*/
+// /*--------------search user-------------------*/
+
+// app.get('/search-user', (req, res) => {
+//     res.render('search');
+// });
+
+// app.post('/searchuser', (req, res) => {
+//     console.log(req.body.temp);
+//     userLogin.find({
+//         fullName: new RegExp(req.body.temp, "i")
+//     }, function (err, user) {
+//         console.log(user);
+//         res.send(user);
+//     });
+// });
+
+// // /*-------------------show profile-----------------------*/
+
+// app.post('/showprofile', (req, res) => {
+//     console.log(req.body);
+//     userLogin.findOne({
+//         _id: req.body["hello"]
+//     }, function (err, user) {
+//         res.redirect("/" + userLoginfullName)
+//         console.log(user);
+//     });
+// });
+
+// app.get("/:customListName", function (req, res) {
+//     userLogin.findOne({
+//         fullName: req.params.customListName
+//     }, function (err, results) {
+//         if (!err) {
+//             if (!results) {
+//                 res.redirect("/");
+//             } else {
+//                 res.render("profile", {
+//                     name: results.fullName,
+//                     image: results.image
+//                 });
+//             }
+//         }
+//     });
+//     console.log(req.params.customListName);
+// });
+
+
+
+
+
+
+
+
+
+
+
+// /*------------register user--------------------*/
+
+
+
+
+// app.get('/profile', (req, res) => {
+//     userLogin.findOne({
+//         _id: req.session.uid
+//     }, (err, user) => {
+//         if (user) {
+
+//         }
+//     });
+// });
+
+// app.post('/upload', function (req, res) {
+//     if (!req.files || Object.keys(req.files).length === 0) {
+//         return res.status(400).send('No files were uploaded.');
+//     }
+//     let sampleFile = req.files.sampleFile;
+//     sampleFile.mv(__dirname + '/public/' + req.session.uid + '.jpg', function (err) {
+//         if (err)
+//             return res.status(500).send(err);
+
+//         res.redirect('/profile');
+//     });
+// });
+
+// listen
 
 app.listen(3000, () => {
     console.log("Server started!");
-});
-
-app.get('/', (req, res)=>{
-    console.log(req.session.uid);
-    
-    res.render('profile', {id: req.session.uid});
-});
-
-app.post('/upload', function (req, res) {
-    if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were uploaded.');
-    }
-    let sampleFile = req.files.sampleFile;
-    sampleFile.mv(__dirname + '/public/'+ req.session.uid +'.jpg', function (err) {
-        if (err)
-            return res.status(500).send(err);
-
-        res.redirect('/profile');
-    });
 });
